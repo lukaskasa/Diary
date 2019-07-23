186284d99e5bb99286c7c6c3f8278ed614787449
Lukas Kasakaitis <lukas.kasakaitis@icloud.com>
2019-07-23 09:06:16 +0200

    Initial Commit

---
+++ Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by Lukas Kasakaitis on 23.07.19.
+//  Copyright © 2019 Lukas Kasakaitis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
